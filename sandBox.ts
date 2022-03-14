c5ad48d959c09a2c06c8ff6d099f6bde61677630
Itachi <Iamitachiuchixa@gmail.com>
2022-03-14 17:43:03 +0500

    first commit

---
+++ sandBox.ts
@@ -0,0 +1,20 @@
+// reading files
+
+// const decoder = new TextDecoder('utf-8');
+// const data = await Deno.readFile('readme.txt');
+// console.log(decoder.decode(data));
+
+const data = await Deno.readTextFile('readme.txt');
+console.log(data);
+
+
+
+// writing files
+// const encoder = new TextEncoder();
+// const  text = encoder.encode('hello again, ninja');
+// await Deno.writeFile('readme.txt', text);
+
+// renaming and removing files
+
+// await Deno.rename('readme.txt', "deleteme.txt");
+// await Deno.remove('deleteme.txt');
